279a5e0547b05b2568dbfa40b76a54a432419370
Jaimee22 <alumno.410564@ies-azarquiel.es>
2023-12-20 21:03:08 +0100

    Login funcional, logout y confirmacion de logout (falta ajustar el renderizado de logout) y ajustar estilos form login y responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,85 +1,139 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoTechRidersNavbar from '../../assets/img/logo-navbar-techriders.png'
-import "./Navbar.css"
+import LogoTechRidersNavbar from '../../assets/img/logo-navbar-techriders.png';
+import { FaUserCircle } from 'react-icons/fa';
+import { TbLogout2 } from 'react-icons/tb';
+import Swal from 'sweetalert2';
+import './Navbar.css';
 
 const Navbar = () => {
+  
+  // Obtiene el token del localStorage
+  const token = localStorage.getItem('token'); 
+
+  const handleLogout = () => {
+    
+    // Token antes de cerrar sesión 
+    console.log('Token antes de cerrar sesión:', token);
+  
+    // Elimina el token del localStorage
+    localStorage.removeItem('token');
+    
+    // token después de cerrar sesión (debería estar borrado)
+    console.log('Se ha cerrado la sesión. Token después de cerrar sesión:', localStorage.getItem('token'));
+
+    // Muestra el mensaje de cierre de sesión exitoso con SweetAlert
+    Swal.fire({
+      icon: 'success',
+      title: 'Sesión cerrada',
+      text: 'Se ha cerrado la sesión de forma exitosa.',
+    });
+  }
+  
+
   return (
     <nav className="navbar navbar-expand-lg bg-dark sticky-top" data-bs-theme="dark">
       <div className="container">
         <a className="navbar-brand" href="/">
-          <img src={LogoTechRidersNavbar} alt="Logo" style={{height:'5rem',paddingTop:'1rem'}}/>
+          <img src={LogoTechRidersNavbar} alt="Logo" style={{ height: '5rem', paddingTop: '1rem' }} />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-                <NavLink className="nav-link" aria-current="page" to="/">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item dropdown">
-                <NavLink
-                  className="nav-link dropdown-toggle"
-                  to="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Seguridad
-                </NavLink>
-                <ul className="dropdown-menu">
-                  <li>
-                    <NavLink className="dropdown-item" to="/login">
-                      Login
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item" to="/registro">
-                      Registro
-                    </NavLink>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/centros">
-                  Centros
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/empresas">
-                  Empresas
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/techriders">
-                  TechRiders
-                </NavLink>
-              </li>
+              <NavLink className="nav-link" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
             <li className="nav-item dropdown">
-              <NavLink className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <NavLink
+                className="nav-link dropdown-toggle"
+                to="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Seguridad
+              </NavLink>
+              <ul className="dropdown-menu">
+                <li>
+                  <NavLink className="dropdown-item" to="/login">
+                    Login
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/registro">
+                    Registro
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/centros">
+                Centros
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/empresas">
+                Empresas
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/techriders">
+                TechRiders
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <NavLink
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Privado
               </NavLink>
               <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-              <li>
-                    <NavLink className="dropdown-item" to="/centros-private">
-                      Centros
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item" to="/empresas-private">
-                      Empresas
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink className="dropdown-item" to="/techriders-private">
-                      TechRiders
-                    </NavLink>
-                  </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/centros-private">
+                    Centros
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/empresas-private">
+                    Empresas
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="dropdown-item" to="/techriders-private">
+                    TechRiders
+                  </NavLink>
+                </li>
               </ul>
             </li>
+            <li className="nav-item iconos-nav">
+              <NavLink className="nav-link" to="/login">
+                <FaUserCircle />
+              </NavLink>
+            </li>
+            {token && (
+              <li className="nav-item iconos-nav">
+                <NavLink className="nav-link" onClick={handleLogout}>
+                  <TbLogout2 />
+                </NavLink>
+              </li>
+            )}
           </ul>
         </div>
       </div>
--- src/pages/public/Login/Login.js
+++ src/pages/public/Login/Login.js
@@ -1,53 +1,81 @@
-import React from 'react'
-import './Login.css'
-import {FaLock, FaEnvelope} from 'react-icons/fa';
+import React, { useState } from 'react';
+import './Login.css';
+import { FaLock, FaEnvelope } from 'react-icons/fa';
 import LogoTechRiders from '../../../assets/img/escudo-negro1.png';
-
+import axios from 'axios';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('https://apitechriders.azurewebsites.net/api/auth/login', {
+        email,
+        password,
+      });
+
+      if (response.status === 200) {
+        const token = response.data.response;
+        console.log('Token:', token);
+        localStorage.setItem('token', token);
+        alert('Inicio de sesión exitoso. Token almacenado.');
+
+      } else {
+        
+        alert('Usuario o contraseña incorrectos');
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+    }
+  };
+
   return (
     <div className="container-principal">
       <div className="container-login-principal">
         <div className="container-login-mediano">
           <div className="foto-login">
-            <img src={LogoTechRiders} alt="img-logo-techriders" style={{width:'35rem'}}/>
+            <img src={LogoTechRiders} alt="img-logo-techriders" style={{ width: '35rem' }} />
           </div>
 
           <form className="form-login">
-            <span className="form-title">
-                Iniciar Sesión
-            </span>
+            <span className="form-title">Iniciar Sesión</span>
 
             <div className="contenedor-login-input">
-              <input className="login-input" type="text" name="email" placeholder="Correo Electrónico" />
+              <input
+                className="login-input"
+                type="text"
+                name="email"
+                placeholder="Correo Electrónico"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
               <span className="focus-input"></span>
               <span className="symbol-input">
-                <FaEnvelope size={15}/>
+                <FaEnvelope size={15} />
               </span>
             </div>
 
             <div className="contenedor-login-input">
-              <input className="login-input" type="password" name="pass" placeholder="Contraseña" />
+              <input
+                className="login-input"
+                type="password"
+                name="pass"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
               <span className="focus-input"></span>
               <span className="symbol-input">
-              <FaLock size={15}/>
+                <FaLock size={15} />
               </span>
             </div>
 
             <div className="container-login-form-btn">
-              <button className="login-form-btn">
-                Inciar Sesión
+              <button type="button" className="login-form-btn" onClick={handleLogin}>
+                Iniciar Sesión
               </button>
             </div>
-
-            {/* <div className="text-center">
-              <span className="txt1">
-                Has olvidado
-              </span>
-              <a className="registrarse-text" href="#">
-                Nombre de Usuario / Contraseña?
-              </a>
-            </div> */}
 
             <div className="text-center">
               <a className="registrarse-text" href="/registro">
